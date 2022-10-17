0ef8febf1f8e538a804ebe80e32fe50c0d5a5bcd
Topandas69 <W0790712@myscc.ca>
2022-10-17 04:37:12 -0700

    Added sample seed data to test

---
+++ Models/SeedData.cs
@@ -0,0 +1,120 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ProCleaner.Data;
+using System;
+using System.Linq;
+
+namespace ProCleaner.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new ProCleanerContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<ProCleanerContext>>()))
+            {
+                // Look for any movies.
+                if (context.Machine.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Machine.AddRange(
+                    new Machine
+                    {
+                        Brand = "Brand 1",
+                        Capacity = "1 cubic feet",
+                        Color = "White",
+                        Material = "Steel",
+                        Weight = 4,
+                        Price = 59.99M
+                    },
+                    new Machine
+                    {
+                        Brand = "Brand 1",
+                        Capacity = "1 cubic feet",
+                        Color = "White",
+                        Material = "Steel",
+                        Weight = 4,
+                        Price = 59.99M
+                    },
+                    new Machine
+                    {
+                        Brand = "Brand 1",
+                        Capacity = "1 cubic feet",
+                        Color = "White",
+                        Material = "Steel",
+                        Weight = 4,
+                        Price = 59.99M
+                    },
+                    new Machine
+                    {
+                        Brand = "Brand 1",
+                        Capacity = "1 cubic feet",
+                        Color = "White",
+                        Material = "Steel",
+                        Weight = 4,
+                        Price = 59.99M
+                    },
+                    new Machine
+                    {
+                        Brand = "Brand 1",
+                        Capacity = "1 cubic feet",
+                        Color = "White",
+                        Material = "Steel",
+                        Weight = 4,
+                        Price = 59.99M
+                    },
+                    new Machine
+                    {
+                        Brand = "Brand 1",
+                        Capacity = "1 cubic feet",
+                        Color = "White",
+                        Material = "Steel",
+                        Weight = 4,
+                        Price = 59.99M
+                    },
+                    new Machine
+                    {
+                        Brand = "Brand 1",
+                        Capacity = "1 cubic feet",
+                        Color = "White",
+                        Material = "Steel",
+                        Weight = 4,
+                        Price = 59.99M
+                    },
+                    new Machine
+                    {
+                        Brand = "Brand 1",
+                        Capacity = "1 cubic feet",
+                        Color = "White",
+                        Material = "Steel",
+                        Weight = 4,
+                        Price = 59.99M
+                    },
+                    new Machine
+                    {
+                        Brand = "Brand 1",
+                        Capacity = "1 cubic feet",
+                        Color = "White",
+                        Material = "Steel",
+                        Weight = 4,
+                        Price = 59.99M
+                    },
+                    new Machine
+                    {
+                        Brand = "Brand 1",
+                        Capacity = "1 cubic feet",
+                        Color = "White",
+                        Material = "Steel",
+                        Weight = 4,
+                        Price = 59.99M
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
