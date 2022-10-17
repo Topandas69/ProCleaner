0f8a8542298b9320a7d112bb6462f92a77fa1ad4
Topandas69 <W0790712@myscc.ca>
2022-10-17 04:34:11 -0700

    Created Dtabase model for machines

---
+++ Models/Machine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProCleaner.Models
+{
+    public class Machine
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string Capacity { get; set; }
+        public string Color { get; set; }
+        public string Material { get; set; }
+        public int Weight { get; set; }
+        public decimal Price { get; set; }
+    }
+}
