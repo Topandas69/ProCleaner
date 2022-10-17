427733b7ad4d9f3444cb7e88aa636a243b539716
Topandas69 <W0790712@myscc.ca>
2022-10-17 04:30:07 -0700

    Created database context

---
+++ Data/ProCleanerContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProCleaner.Models;
+
+namespace ProCleaner.Data
+{
+    public class ProCleanerContext : DbContext
+    {
+        public ProCleanerContext(DbContextOptions<ProCleanerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Machine> Machine { get; set; } 
+    }
+}
