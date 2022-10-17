098d70d04f12b1eb2dec90f93ccf06c9aa8eb8d2
Topandas69 <W0790712@myscc.ca>
2022-10-17 06:36:04 -0700

    new field rating migrations & updated database

--- Migrations/20221017132632_Rating.Designer.cs
+++ Migrations/20221017132632_Rating.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProCleaner.Data;
 
 namespace ProCleaner.Migrations
 {
     [DbContext(typeof(ProCleanerContext))]
-    partial class ProCleanerContextModelSnapshot : ModelSnapshot
+    [Migration("20221017132632_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20221017132632_Rating.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProCleaner.Migrations
+{
+    public partial class Rating : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Rating",
+                table: "Machine",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Machine");
+        }
+    }
+}
