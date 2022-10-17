f1067289a6cedef8221ffc31ada25dcf63acdb6c
Topandas69 <W0790712@myscc.ca>
2022-10-17 04:30:46 -0700

    database migrations

---
+++ Migrations/20221017110905_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProCleaner.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Machine",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Brand = table.Column<string>(nullable: true),
+                    Capacity = table.Column<string>(nullable: true),
+                    Color = table.Column<string>(nullable: true),
+                    Material = table.Column<string>(nullable: true),
+                    Weight = table.Column<int>(nullable: false),
+                    Price = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Machine", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Machine");
+        }
+    }
+}
