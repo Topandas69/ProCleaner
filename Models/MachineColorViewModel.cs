7ebd6b1f7d53f6da2883775c9d5e29d0ded423ef
Topandas69 <W0790712@myscc.ca>
2022-10-17 06:01:30 -0700

    Created a view Model for a new search filter

---
+++ Models/MachineColorViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace ProCleaner.Models
+{
+    public class MachineColorViewModel
+    {
+        public List<Machine> Machines { get; set; }
+        public SelectList Colors { get; set; }
+        public string MachineColor { get; set; }
+        public string SearchString { get; set; }
+    }
+}
